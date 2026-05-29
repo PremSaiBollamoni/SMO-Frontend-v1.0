ae67968f294e48a8d5be32cfca3df8b5962324a2
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-05-29 14:53:22 +0530

    config: Update backend URL to local dev (192.168.0.102)

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,9 +1,8 @@
 /// Application configuration constants
-/// Using deployed backend URL
-// const String fallbackBaseUrl = 'http://192.168.0.102:8080'; // WiFi debugging - phone on same network
-// const String fallbackBaseUrl = 'http://10.0.2.2:8080'; // USB debugging (emulator only)
-const String fallbackBaseUrl = 'http://192.168.0.110:8080'; // Local dev - WiFi
+/// Using local backend URL for development
 // const String fallbackBaseUrl = 'https://smobza.thegttech.com/smo'; // Deployed backend
+// const String fallbackBaseUrl = 'http://192.168.1.6:8080'; // Old WiFi
+const String fallbackBaseUrl = 'http://192.168.0.102:8080'; // Local dev - WiFi
 const String appVersion = '1.0';
 const String appName = 'SMO System';
 const String appSubtitle = 'Sewing Machine Operations';
