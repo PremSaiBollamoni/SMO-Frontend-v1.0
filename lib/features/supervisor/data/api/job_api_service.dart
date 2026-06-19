2e822dffb27d2e45bcfa6c77f3483e30b76dbc2c
Prem Sai Bollamoni <premsai200804@gmail.com>
2026-06-19 13:42:35 +0530

    feat: implement job_api_service screen/widget

---
+++ lib/features/supervisor/data/api/job_api_service.dart
@@ -0,0 +1,33 @@
+import '../../../../core/network/api_client.dart';
+import '../models/job_assignment_model.dart';
+
+class JobApiService {
+  final _dio = ApiClient().dio;
+
+  Future<ScanBundleResponse> scanBundle({
+    required String barcode,
+    required int wsId,
+    required int empId,
+    required int assignedBy,
+    int? bundleQty,
+  }) async {
+    final res = await _dio.post('/api/jobs/scan', data: {
+      'barcode': barcode,
+      'wsId': wsId,
+      'empId': empId,
+      'assignedBy': assignedBy,
+      if (bundleQty != null) 'bundleQty': bundleQty,
+    });
+    return ScanBundleResponse.fromJson(res.data);
+  }
+
+  Future<List<ActiveJob>> getActiveJobs() async {
+    final res = await _dio.get('/api/jobs/active');
+    return (res.data as List).map((j) => ActiveJob.fromJson(j)).toList();
+  }
+
+  Future<List<ActiveJob>> getActiveJobsByStation(int wsId) async {
+    final res = await _dio.get('/api/jobs/station/$wsId/active');
+    return (res.data as List).map((j) => ActiveJob.fromJson(j)).toList();
+  }
+}
